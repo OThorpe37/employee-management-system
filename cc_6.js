7eca770992dc7791f6496d0ff3aee44ba3031212
Olivia Thorpe <othorpe@usf.edu>
2026-02-23 17:56:12 -0500

    Added Company Class with Employee Property

--- cc_6.js
+++ cc_6.js
@@ -21,3 +21,15 @@ const employee2 = new Employee('Bob Lee', 'Sales');
 const employee3 = new Employee ('Alice Smith', 'Marketing');
 const manager1 = new Manager('Sarah Jones', 'IT', 5);
 const manager2 = new Manager('David Brown', 'HR', 3);
+
+class Company {
+    constructor() {
+        this.employees = [];
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    listEmployees() {
+        this.employees.forEach (employee => {console.log(employee.describe())});
+    }
+}
